55144592bcb5739127b05670458a816517fc5c26
kriswell <1910940451@qq.com>
2020-06-20 16:55:52 +0800

    test jenkins

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,43 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import './App.css';
+import React, { useState, useEffect, useReducer } from 'react'
+import './App.css'
 
 type actionType = {
-  type: 'increment' | 'decrement'
+	type: 'increment' | 'decrement'
 }
 
 type State = {
-  count: number
+	count: number
 }
 
 const reducer = (state: State, action: actionType) => {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + 1 }
-    case 'decrement':
-      return { count: state.count - 1 }
-    default:
-      throw new Error()
-  }
+	switch (action.type) {
+		case 'increment':
+			return { count: state.count + 1 }
+		case 'decrement':
+			return { count: state.count - 1 }
+		default:
+			throw new Error()
+	}
 }
 
 const initialCount = 3
 
 const init = (initialCount: number) => ({
-  count: initialCount
+	count: initialCount,
 })
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialCount, init)
+	const [count, dispatch] = useReducer(reducer, initialCount, init)
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-    }, 1000)
-    return () => {
-      clearInterval(timer)
-      console.log('我清楚了')
-    }
-  }, [])
+	useEffect(() => {
+		const timer = setInterval(() => {}, 1000)
+		return () => {
+			clearInterval(timer)
+			console.log('我清楚了 ')
+		}
+	}, [])
 
-  return (
-    <div>{count}</div>
-  );
+	return <div>{count}</div>
 }
 
-export default App;
+export default App
