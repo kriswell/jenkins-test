c921eae892727905b4faa3e76817f482c12c9c57
kriswell <1910940451@qq.com>
2020-06-20 16:46:44 +0800

    test jenkins

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useReducer } from 'react';
+import './App.css';
+
+type actionType = {
+  type: 'increment' | 'decrement'
+}
+
+type State = {
+  count: number
+}
+
+const reducer = (state: State, action: actionType) => {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
+
+const initialCount = 3
+
+const init = (initialCount: number) => ({
+  count: initialCount
+})
+
+function App() {
+  const [count, dispatch] = useReducer(reducer, initialCount, init)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+    }, 1000)
+    return () => {
+      clearInterval(timer)
+      console.log('我清楚了')
+    }
+  }, [])
+
+  return (
+    <div>{count}</div>
+  );
+}
+
+export default App;
