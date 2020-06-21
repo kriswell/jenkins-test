5432aef4139c248c6c3c5c0b20545ca7156d5926
kriswell <1910940451@qq.com>
2020-06-21 14:48:18 +0800

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,8 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import './App.css'
 
-type actionType = {
-	type: 'increment' | 'decrement'
-}
-
-type State = {
-	count: number
-}
-
-const reducer = (state: State, action: actionType) => {
-	switch (action.type) {
-		case 'increment':
-			return { count: state.count + 1 }
-		case 'decrement':
-			return { count: state.count - 1 }
-		default:
-			throw new Error()
-	}
-}
-
-const initialCount = 3
-
-const init = (initialCount: number) => ({
-	count: initialCount,
-})
-
-function App() {
-	const [count, dispatch] = useReducer(reducer, initialCount, init)
-
-	useEffect(() => {
-		const timer = setInterval(() => {}, 1000)
-		return () => {
-			clearInterval(timer)
-			console.log('我清楚了 ')
-		}
-	}, [])
-
-	return <div>{count}</div>
+const App: React.FC = () => {
+	return <div>我是童秋琦</div>
 }
 
 export default App
