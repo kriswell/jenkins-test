c7072c73660fdc625cd50c3c9ddad16221ffdc0e
kriswell <caoweixiaosa@163.com>
2020-11-06 17:20:50 +0800

    hhh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React, { useState, useEffect, useReducer } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
+const restTime = Math.floor(
+  (new Date("2020-11-06 17:30").getTime() - new Date().getTime()) / 1000
+);
 const App: React.FC = () => {
-	return <div>我是童秋琦</div>
-}
+  const [time, setTime] = useState(restTime);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [time]);
 
-export default App
+  return (
+    <div>
+      兄弟盟，还有
+      <span style={{ fontSize: "28px", fontWeight: "bold" }}>{time}</span>
+      秒我们就可以操作啦!!!
+    </div>
+  );
+};
+
+export default App;
