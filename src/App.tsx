827f89a6cfc560174e867e40e52597f409c0bcb7
kriswell <caoweixiaosa@163.com>
2020-11-25 17:26:57 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const restTime = Math.floor(
-  (new Date("2020-11-06 17:30").getTime() - new Date().getTime()) / 1000
+  (new Date("2020-11-27 17:30").getTime() - new Date().getTime()) / 1000
 );
 const App: React.FC = () => {
   const [time, setTime] = useState(restTime);
+
   useEffect(() => {
-    const timer = setInterval(() => {
-      setTime(time - 1);
-    }, 1000);
-    return () => clearInterval(timer);
+    if (time > 0) {
+      const timer = setInterval(() => {
+        setTime(time - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
   }, [time]);
 
-  return <div>jenkins test1111</div>;
+  return <div className="test">操作倒计时：{time} s</div>;
 };
 
 export default App;
