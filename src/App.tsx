c51da1d937c0db937ab0bdafa26cdb43910724f5
kriswell <caoweixiaosa@163.com>
2021-09-23 15:58:11 +0800

    111

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
 const restTime = Math.floor(
-  (new Date("2020-12-18T17:30").getTime() - new Date().getTime()) / 1000
-);
+  (new Date('2020-12-18T17:30').getTime() - new Date().getTime()) / 1000
+)
 const App: React.FC = () => {
-  const [time, setTime] = useState(restTime);
+  const [time, setTime] = useState(restTime)
 
   useEffect(() => {
-    let timer: any;
+    let timer: any
     if (time > 0) {
       timer = setInterval(() => {
-        setTime(time - 1);
-      }, 1000);
-      return () => clearInterval(timer);
+        setTime(time - 1)
+      }, 1000)
+      return () => clearInterval(timer)
     } else {
-      setTime(0);
-      clearInterval(timer);
+      setTime(0)
+      clearInterval(timer)
     }
-  }, [time]);
+  }, [time])
 
   useEffect(() => {
-    const a = (window.screen.width / 1920) * 100;
-    document.documentElement.style.fontSize = a + "px";
-  }, []);
-  return <div className="test">测试：{time} s</div>;
-};
+    const a = (window.screen.width / 1920) * 100
+    document.documentElement.style.fontSize = a + 'px'
+  }, [])
+  return <div className="test">测试1111111：{time} s</div>
+}
 
-export default App;
+export default App
